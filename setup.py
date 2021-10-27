ac444ccbc7fd16c5f3dfefc666c1622beca8f770
Navindu Dananga <navindudananga123@gmail.com>
2021-10-27 22:39:26 +0530

    version updated to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vdataset",
-    version="0.0.1",
+    version="0.0.2",
     author="Navindu Dananga",
     author_email="navindum@protonmail.com",
     license="MIT",
